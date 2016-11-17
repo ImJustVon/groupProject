1db7541e73fa452ffed8adc4de31e0c550359d63
Pat Hiniker <pathiniker@Pats-MacBook-Pro.local>
2016-11-17 10:20:06 -0600

    angular routing is set up

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -1,3 +1,12 @@
 angular.module('routeApp')
         .config(function($routeProvider, $locationProvider) {
-          $routeProvider.when('/')
+          $routeProvider.when('/home', {
+            templateUrl: 'views/home.html',
+          }).when('/login', {
+            templateUrl: 'views/login.html',
+          }).otherwise({
+              redirectTo: '/home',
+          });
+
+          $locationProvider.html5mode(true);
+        }); // end routeApp
