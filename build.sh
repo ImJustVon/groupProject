1336f4d8ea9c48f56117051c11a69f5980298395
Vaughn Prosser <vaughnprosser@gmail.com>
2016-11-18 09:33:01 -0600

    bootstrap not bootstra

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendors;
 cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
 
 cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js public/vendors;
-cp node_modules/angular-ui-bootstra/dist/ui-bootstrap-csp.css public/vendors;
+cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css public/vendors;
