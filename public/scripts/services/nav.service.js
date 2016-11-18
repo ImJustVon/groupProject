c50918e18dc72d455bdd4e14154e3cb01499f112
Barrett Amsrud <barrettamsrud@gmail.com>
2016-11-18 10:53:16 -0600

    Wrote search function

--- public/scripts/services/nav.service.js
+++ public/scripts/services/nav.service.js
@@ -1,8 +1,17 @@
 angular.module('routeApp')
   .service('NavService', NavService)
 
-function NavService () {
-  
+//  Service holds data accessed across multiple controllers
+function NavService ($http) {
+
   var nav = this;
+
+  //  Search database
+  nav.search = function(searchTerm) {
+    return $http.get('/search/' + searchTerm).then(function(result) {
+      //  Returns array of objects {_id, name, category, options, score, overRide, overRideValue, tags}
+      return result.data;
+    });
+  }
 
 }
