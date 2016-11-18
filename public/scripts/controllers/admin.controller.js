a1e485d5e3275709319bdbe10e7f357595982df8
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2016-11-18 09:59:11 -0600

    modal admin controller created

--- public/scripts/controllers/admin.controller.js
+++ public/scripts/controllers/admin.controller.js
@@ -2,13 +2,14 @@ angular.module('routeApp').controller('AdminController', AdminController);
 
 function AdminController($uibModal, $log, $document) {
   var admin = this;
-  admin.items = ['item1', 'item2', 'item3'];
+  //admin.items = ['item1', 'item2', 'item3'];
 
   admin.animationsEnabled = true;
 
   admin.open = function (size, parentSelector) {
     var modalInstance = $uibModal.open({
       templateUrl: 'views/demoModal.html',
+      controller: 'ModalAdminController as modal';
       // resolve: {
       //   items: function () {
       //     return admin.items;
