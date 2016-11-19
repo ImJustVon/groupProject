513363eb6f01d87b25e53b3a1f9395847ac27a5b
Barrett Amsrud <barrettamsrud@gmail.com>
2016-11-19 09:58:15 -0600

    Added  functions to FoodService

--- public/scripts/services/food.service.js
+++ public/scripts/services/food.service.js
@@ -4,13 +4,64 @@ angular.module('routeApp')
 //  Service holds data accessed across multiple controllers
 function FoodService ($http) {
 
-  var nav = this;
+  var food = this;
+
+  //  Get all foods
+  food.getFoods = function() {
+    return $http.get('/food').then(function(response) {
+      //  Returns array of objects {_id, name, category, options, score, overRide, overRideValue, tags}
+      return response.data;
+    });
+  }
+
+  //  Post a food, takes object {name, category, options, score, overRide, overRideValue, tags}
+  food.postFood = function(foodData) {
+    return $http({
+      method: 'POST',
+      url: '/food',
+      data: foodData
+    }).then(function(response) {
+      console.log('POST successful:', response.data);
+      return response.data;
+    });
+  }
+
+  //  Update a food, takes object {_id, name, category, options, score, overRide, overRideValue, tags}
+  food.updateFood = function(foodData) {
+    return $http({
+      method: 'PUT',
+      url: '/food/' + foodData._id,
+      data: foodData
+    }).then(function(response) {
+      console.log('PUT successful:', response.data);
+      return response.data;
+    });
+  }
+
+  //  Delete food, takes object {_id, name, category, options, score, overRide, overRideValue, tags}
+  food.deleteFood = function(foodData) {
+    return $http({
+      method: 'DELETE',
+      url: '/food/' + foodData.id,
+      data: foodData
+    }).then(function(response) {
+      console.log('DELETE successful');
+    });
+  }
+
+  //  Get foods by category
+  food.getCategory = function(catName) {
+    return $http.get('food/category/' + catName).then(function(response) {
+      //  Returns array of objects {_id, name, category, options, score, overRide, overRideValue, tags}
+      return response.data;
+    })
+  }
 
   //  Search database
-  nav.search = function(searchTerm) {
-    return $http.get('/search/' + searchTerm).then(function(result) {
+  food.search = function(searchTerm) {
+    return $http.get('/food/search/' + searchTerm).then(function(response) {
       //  Returns array of objects {_id, name, category, options, score, overRide, overRideValue, tags}
-      return result.data;
+      return response.data;
     });
   }
 
