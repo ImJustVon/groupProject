c7240bf4bfc13c68f14b9015350b20af6416507b
Barrett Amsrud <barrettamsrud@gmail.com>
2016-11-20 21:17:55 -0600

    Added basic get and post for options

--- public/scripts/services/options.service.js
+++ public/scripts/services/options.service.js
@@ -3,4 +3,26 @@ angular.module('routeApp')
 
 function OptionService($http) {
 
+  var option = this;
+
+  //  Get all options in database
+  option.getOptions = function() {
+    return $http.get('/options').then(function(response) {
+      //  Returns array of objects {_id, name, score, type}
+      return response.data;
+    });
+  }
+
+  //  Post an option, needs {name, score, type}
+  option.postOption = function(optionData) {
+    return $http({
+      method: 'POST',
+      url: '/options',
+      data: optionData
+    }).then(function(response) {
+      //  Returns array of objects {_id, name, score, type}
+      console.log('POST successful:', response.data);
+    });
+  }
+
 }
