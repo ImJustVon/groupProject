5e13a6dee153b42546ac3de9fd24c97192339a06
Barrett Amsrud <barrettamsrud@gmail.com>
2016-11-21 10:59:08 -0600

    Admin modal function to add category

--- public/scripts/controllers/newCategory.admin.controller.js
+++ public/scripts/controllers/newCategory.admin.controller.js
@@ -1,7 +1,20 @@
 angular.module('routeApp').controller('NewCategoryAdminController', NewCategoryAdminController);
 
-function NewCategoryAdminController() {
+function NewCategoryAdminController(CategoryService) {
 
   var admin = this;
-  
+
+  //  admin.name -> new category name
+
+  //  Post the new category
+  admin.addCategory = function() {
+    var categoryData = {
+      name: admin.name
+    }
+    CategoryService.postCategory(categoryData).then(function(response) {
+      //  Do something
+      console.log('Response:', response);
+    });
+  }
+
 }
