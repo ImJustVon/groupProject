2f95b6704584fc2e58f6ff4c2efd324449e821c7
Pat Hiniker <hini0027@umn.edu>
2016-11-21 14:30:05 -0600

    add option type

--- public/scripts/controllers/newOption.admin.controller.js
+++ public/scripts/controllers/newOption.admin.controller.js
@@ -29,7 +29,7 @@ function NewOptionAdminController(OptionService, $uibModalInstance) {
   admin.addOptionType = function() {
     OptionService.addOptionType(admin.typeData).then(function(response) {
       console.log('POSTed option type:', response);
-      admin.getOptionTypes();
+      // admin.getOptionTypes();
     });
   }
 
