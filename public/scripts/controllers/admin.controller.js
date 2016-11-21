cc3d0e076bdce3a3322fc8b61f1e1d1999c4339d
Pat Hiniker <hini0027@umn.edu>
2016-11-20 19:43:09 -0600

    adding to admin modals

--- public/scripts/controllers/admin.controller.js
+++ public/scripts/controllers/admin.controller.js
@@ -68,17 +68,10 @@ function AdminController($uibModal, $log, $document) {
     });
   } // end admin.confirmDelete
 
-  admin.closeModal = function () {
-    var modalInstance = $uibModal.close({
-      templateUrl: 'views/modals/confirmDeleteModal.html',
-      // controller: 'ModalAdminController as modal',
-      // resolve: {
-      //   items: function () {
-      //     return admin.items;
-      //   }
-      // }
-    });
-  } // end admin.closeModal
+
+  admin.cancel = function () {
+    var modalInstance = $uibModal.close();
+  };
 
 
 
