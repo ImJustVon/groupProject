870f96b1b4f06782e1f5c2b83ab38ce4602a07a9
Barrett Amsrud <barrettamsrud@gmail.com>
2016-11-22 10:54:02 -0600

    Added function to get and post locations

--- public/scripts/controllers/newLocation.admin.controller.js
+++ public/scripts/controllers/newLocation.admin.controller.js
@@ -1,30 +1,31 @@
 angular.module('routeApp').controller('NewLocationAdminController', NewLocationAdminController);
 
 function NewLocationAdminController($uibModalInstance) {
-// LocationService, 
+// LocationService,
   var admin = this;
 
   //  admin.name -> new location name
-  //
-  // //  Post the new location
-  // admin.addLocation = function() {
-  //   var locationData = {
-  //     name: admin.name
-  //   }
-  //   LocationService.postLocation(locationData).then(function(response) {
-  //     //  Do something
-  //     console.log('Response:', response);
-  //     admin.getLocations();
-  //   });
-  // }
-  //
-  // //  Get all locations
-  // admin.getLocations = function() {
-  //   LocationService.getLocations().then(function(response) {
-  //     console.log('GET successful:', response);
-  //   });
+
+  //  Post the new location
+  admin.addLocation = function() {
+    var locationData = {
+      name: admin.name
+    }
+    LocationService.postLocation(locationData).then(function(response) {
+      //  Do something
+      console.log('Response:', response);
+      admin.getLocations();
+    });
   // }
 
+  //  Get all locations
+  admin.getLocations = function() {
+    LocationService.getLocations().then(function(response) {
+      console.log('GET successful:', response);
+    });
+  }
+
+  //  Close modal
   admin.cancel = function() {
     $uibModalInstance.close();
   }
--- public/scripts/services/location.service.js
+++ public/scripts/services/location.service.js
@@ -4,5 +4,24 @@ angular.module('routeApp')
 function LocationService($http) {
 
   var location = this;
-  
+
+  //  Get all locations
+  location.getLocations = function() {
+    return $http.get('/location').then(function(response) {
+      //  Returns array of objects {_id, name}
+      return response.data;
+    });
+  }
+
+  //  Post a location, takes an object {name}
+  location.postLocation = function(locationData) {
+    return $http({
+      method: 'POST',
+      url: '/location',
+      data: locationData
+    }).then(function(response) {
+      return response.data;
+    });
+  }
+
 }
