d9d2741bb1e83e5e11be65bde2b03cfe50d87f3a
Vaughn Prosser <vaughnprosser@gmail.com>
2016-11-23 13:57:38 -0600

    added imageLocation

--- models/category.js
+++ models/category.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Category = mongoose.model('Category', {
   name: { type: String, required: true },
+  imageLocation: { type: String, required: true },
 });
 
 module.exports = Category;
