65a792571b552bb1d41ca3bfbcdcfc43843e1d41
Pat Hiniker <hini0027@umn.edu>
2016-11-29 14:08:02 -0600

    issue with option types repeating in edit modal

--- public/scripts/controllers/editOption.admin.controller.js
+++ public/scripts/controllers/editOption.admin.controller.js
@@ -1,8 +1,31 @@
 angular.module('routeApp').controller('EditOptionAdminController', EditOptionAdminController);
 
-function EditOptionAdminController($uibModalInstance) {
+function EditOptionAdminController($uibModalInstance, OptionService) {
 
   var admin = this;
+
+  // Get option types for food creation/updating
+  admin.getOptionTypes = function() {
+    OptionService.getOptionTypes().then(function(response) {
+      console.log('Got option types, in admin.types:', response);
+      admin.types = response;
+    });
+  }
+
+  //  Get options for food creation/updating
+  admin.getOptions = function() {
+    OptionService.getOptions().then(function(response) {
+      console.log('GET successful:', response);
+      admin.options = response;
+      console.log('Options:', admin.options);
+    });
+  }
+
+  admin.getOptionTypes();
+  admin.getOptions();
+
+
+
 
   admin.cancel = function() {
     $uibModalInstance.close();
