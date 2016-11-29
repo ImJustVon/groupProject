0935407321d62ff9f45c79debf509281a24c051e
Pat Hiniker <hini0027@umn.edu>
2016-11-29 14:02:59 -0600

    populating edit categories modal

--- public/scripts/controllers/editCategory.admin.controller.js
+++ public/scripts/controllers/editCategory.admin.controller.js
@@ -1,11 +1,22 @@
 angular.module('routeApp').controller('EditCategoryAdminController', EditCategoryAdminController);
 
-function EditCategoryAdminController($uibModalInstance) {
+function EditCategoryAdminController($uibModalInstance, CategoryService) {
 
   var admin = this;
+
+  //  Get categories for food creation/updating
+  admin.getCategories = function() {
+    CategoryService.getCategories().then(function(response) {
+      console.log('GET successful:', response);
+      admin.categories = response;
+    });
+  }
+
 
   admin.cancel = function() {
     $uibModalInstance.close();
   }
+
+  admin.getCategories();
 
 }
