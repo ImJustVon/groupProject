215f0c69594856ace9af738005653eb3124b798a
Vaughn Prosser <vaughnprosser@gmail.com>
2016-12-01 13:32:57 -0600

    search

--- routes/food.js
+++ routes/food.js
@@ -35,7 +35,7 @@ router.get('/search/:word', function (req, res) {
   // });
 
   //checks the name for a match and the tags array for a matching index then returns the document
-  Food.find({ $or: [{ name: { $regex: search } }, { tags: req.params.word }, { location: req.params.word }] }).then(function (dataFromTheDatabase) {
+  Food.find({ $or: [{ name: { $regex: search } }, { tags: { $regex: search } }, { location: { $regex: search } }] }).then(function (dataFromTheDatabase) {
     console.log('Search result ', dataFromTheDatabase);
     res.send(dataFromTheDatabase);
   });
