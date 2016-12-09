1930028a092e2a8aeb99b76f7754ada33af69f0e
Vaughn Prosser <vaughnprosser@gmail.com>
2016-12-09 09:51:54 -0600

    changed app.config

--- public/scripts/app.config.js
+++ public/scripts/app.config.js
@@ -1,41 +1,38 @@
 angular.module('routeApp')
-        .config(function($routeProvider, $locationProvider) {
+        .config(function ($routeProvider, $locationProvider) {
           $routeProvider.when('/home', {
             templateUrl: 'views/home.html',
-            controller: 'HomeController as home'
+            controller: 'HomeController as home',
           }).when('/login', {
             templateUrl: 'views/login.html',
-            controller: 'LoginController as login'
+            controller: 'LoginController as login',
           }).when('/locations', {
             templateUrl: 'views/locations.html',
-            controller: 'LocationController as location'
+            controller: 'LocationController as location',
           }).when('/user-landing', {
             templateUrl: 'views/user-landing.html',
-            controller: 'UserLandingController as user'
-          }).when('/chosen-food-hotdog', {
-            templateUrl: 'views/chosen-food-hotdog.html',
-            //controller: ''
+            controller: 'UserLandingController as user',
           }).when('/admin', {
             templateUrl: 'views/admin.html',
-            controller: 'AdminController as admin'
+            controller: 'AdminController as admin',
           }).when('/results', {
             templateUrl: 'views/results.html',
-            controller: 'ResultsController as results'
+            controller: 'ResultsController as results',
           }).when('/categories', {
             templateUrl: 'views/categories.html',
-            controller: 'CategoriesController as cat'
+            controller: 'CategoriesController as cat',
           }).when('/report', {
             templateUrl: 'views/report.html',
-            controller: 'ReportController as report'
+            controller: 'ReportController as report',
           }).when('/chosen-food', {
             templateUrl: 'views/chosen-food.html',
-            controller: 'FoodController as food'
+            controller: 'FoodController as food',
           }).when('/user-feedback', {
             templateUrl: 'views/user-feedback.html',
-            controller: 'ReportController as report'
+            controller: 'ReportController as report',
           }).otherwise({
               redirectTo: '/home',
-          });
+            });
 
           $locationProvider.html5Mode(true);
         }); // end routeApp
